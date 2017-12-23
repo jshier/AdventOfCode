62993e768572f7ee19221a07c995bad81d171442
Jon Shier <jon@jonshier.com>
2017-12-23 16:19:50 -0500

    Finish Day 22.

--- AdventOfCode/Day22.swift
+++ AdventOfCode/Day22.swift
@@ -10,22 +10,40 @@ import Foundation
 
 final class Day22: Day {
     override func perform() {
-//        let fileInput = String.input(forDay: 22)
-        let testInput = """
-                        ..#
-                        #..
-                        ...
-                        """
-        let input = testInput
+        let fileInput = String.input(forDay: 22)
+//        let testInput = """
+//                        ..#
+//                        #..
+//                        ...
+//                        """
+        let input = fileInput
         let grid = Grid(content: input)
-        print(grid)
+        grid.burst()
+
+        stageOneOutput = "\(grid.infections)"
+        
+//        var previousEvolution = grid
+//        var infections = 0
+//        for i in 1...10_000_000 {
+//            let evolution = previousEvolution.evolve()
+//            if evolution.didInfect { infections += 1 }
+//            previousEvolution = evolution.grid
+//            if i % 100_000 == 0 { print("Evolutions: \(i), Infections: \(infections)") }
+//        }
+//
+        let evolutionGrid = Grid(content: input)
+        evolutionGrid.evolve()
+        
+        stageTwoOutput = "\(evolutionGrid.infections)"
     }
     
     
 }
 
-struct Grid {
-    let content: [Point: Node]
+final class Grid {
+    private var content: [Point: Node]
+    private var virus: Virus
+    private(set) var infections = 0
     
     init(content: String) {
         let lines = content.split(separator: "\n")
@@ -33,29 +51,89 @@ struct Grid {
         assert(nodes.count % 2 == 1 && nodes[0].count % 2 == 1)
         let middle = nodes.count / 2
         var nodeMap: [Point: Node] = [:]
-        for (i, line) in nodes.enumerated() {
-            for (j, node) in line.enumerated() {
-                let point = Point(j, i)
+        for (y, line) in nodes.enumerated() {
+            for (x, node) in line.enumerated() {
+                let offsetX = x - middle
+                let offsetY = middle - y
+                let point = Point(offsetX, offsetY)
+                nodeMap[point] = node
             }
         }
         
-        self.content = [:]
+        self.content = nodeMap
+        virus = Virus(position: Point(0, 0), facing: .up)
     }
     
+    func burst() {
+        for _ in 0..<10_000 {
+            let currentNode = self[virus.position]
+            if currentNode == .clean { infections += 1 }
+            let currentPosition = virus.position
+            virus = virus.simpleMove(from: self[currentPosition])
+            self[currentPosition] = self[currentPosition].swap()
+            
+        }
+    }
+    
+    func evolve() {
+        for _ in 0..<10_000_000 {
+            let currentNode = self[virus.position]
+            if currentNode == .weakened { infections += 1 }
+            let currentPosition = virus.position
+            virus = virus.complexMove(from: self[currentPosition])
+            self[currentPosition] = self[currentPosition].evolve()
+        }
+    }
+    
+//    func evolve() {
+//        var newContent = content
+//        let newVirus = virus.complexMove(from: self[virus.position])
+//        let original = self[virus.position]
+//        let didInfect = (original == .weakened)
+//        newContent[virus.position] = self[virus.position].evolve()
+//        let grid = Grid(content: newContent, virus: newVirus)
+//
+//        return (grid, didInfect)
+//    }
+    
     subscript(point: Point) -> Node {
-        return content[point]!
+        get {
+            return content[point] ?? .clean
+        }
+        set {
+            content[point] = newValue
+        }
     }
 }
 
 extension Grid: CustomStringConvertible {
     var description: String {
-        return ""
+        return content.description
     }
 }
 
 enum Node: Character {
     case clean = "."
     case infected = "#"
+    case weakened = "W"
+    case flagged = "F"
+    
+    func swap() -> Node {
+        switch self {
+        case .clean: return .infected
+        case .infected: return .clean
+        default: fatalError("Invalid value for swap.")
+        }
+    }
+    
+    func evolve() -> Node {
+        switch self {
+        case .clean: return .weakened
+        case .weakened: return .infected
+        case .infected: return .flagged
+        case .flagged: return .clean
+        }
+    }
 }
 
 extension Node: CustomStringConvertible {
@@ -74,9 +152,33 @@ struct Point: Hashable {
     }
 }
 
+extension Point: CustomStringConvertible {
+    var description: String {
+        return "(\(x), \(y))"
+    }
+}
+
+func + (lhs: Point, rhs: (x: Int, y: Int)) -> Point {
+    let x = lhs.x + rhs.x
+    let y = lhs.y + rhs.y
+    return Point(x, y)
+}
+
 struct Virus {
     let position: Point
-    let direction: Direction
+    let facing: Direction
+    
+    func simpleMove(from node: Node) -> Virus {
+        let newDirection = facing.simpleTurn(from: node)
+        let newPosition = position + newDirection.forwardOffset
+        return Virus(position: newPosition, facing: newDirection)
+    }
+    
+    func complexMove(from node: Node) -> Virus {
+        let newDirection = facing.complexTurn(from: node)
+        let newPosition = position + newDirection.forwardOffset
+        return Virus(position: newPosition, facing: newDirection)
+    }
 }
 
 enum Direction {
@@ -99,5 +201,44 @@ enum Direction {
         case (.right, .right), (.left, .left): return .down
         default: fatalError("Invalid turn.")
         }
+    }
+    
+    var reverse: Direction {
+        switch self {
+        case .up: return .down
+        case .down: return .up
+        case .left: return .right
+        case .right: return .left
+        }
+    }
+    
+    func simpleTurn(from node: Node) -> Direction {
+        switch node {
+        case .clean: return turn(.left)
+        case .infected: return turn(.right)
+        default: fatalError("Invalid simple turn.")
+        }
+    }
+    
+    func complexTurn(from node: Node) -> Direction {
+        switch node {
+        case .clean: return turn(.left)
+        case .weakened: return self
+        case .infected: return turn(.right)
+        case .flagged: return reverse
+        }
+    }
+}
+
+extension Array {
+    func count(where predicate: (Element) -> Bool) -> Int {
+        var count = 0
+        for element in self {
+            if predicate(element) {
+                count += 1
+            }
+        }
+        
+        return count
     }
 }
