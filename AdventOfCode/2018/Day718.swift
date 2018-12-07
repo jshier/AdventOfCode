e4a72636bb6fcb31346552b31592f8379b2b13d2
Jon Shier <jon@jonshier.com>
2018-12-07 18:59:28 -0500

    Prep 718.

---
+++ AdventOfCode/2018/Day718.swift
@@ -0,0 +1,24 @@
+//
+//  Day718.swift
+//  AdventOfCode
+//
+//  Created by Jon Shier on 12/7/18.
+//  Copyright © 2018 Jon Shier. All rights reserved.
+//
+
+import Foundation
+
+final class Day718: Day {
+    override func perform() {
+        //        let input = String.input(forDay: 7)
+        let input = """
+                    Step C must be finished before step A can begin.
+                    Step C must be finished before step F can begin.
+                    Step A must be finished before step B can begin.
+                    Step A must be finished before step D can begin.
+                    Step B must be finished before step E can begin.
+                    Step D must be finished before step E can begin.
+                    Step F must be finished before step E can begin.
+                    """
+    }
+}
