15b946c91741e5041910c7eff984b30ebcf6dc09
Jon Shier <jon@jonshier.com>
2019-12-19 18:29:45 -0500

    Start Day 1819 refactor.

--- AdventOfCode/2019/Day1819.swift
+++ AdventOfCode/2019/Day1819.swift
@@ -85,6 +85,13 @@ final class Day1819: Day {
 
         addPoints(adjacentTo: atPoint)
 
+        let allPaths: [Point: [Point: [WeightedGraph<Point, Int>.E]]] = keys.reduce(into: [:]) { output, element in
+            let pathsToOtherKeys: [Point: [WeightedGraph<Point, Int>.E]] = keys.subtracting([element]).reduce(into: [:]) { subOutput, subElement in
+                subOutput[subElement] = graph.bfs(from: element, to: subElement)
+            }
+            output[element] = pathsToOtherKeys
+        }
+
 //        print(graph)
 
         // Find all routes from point to available keys
@@ -92,55 +99,45 @@ final class Day1819: Day {
         // Remove destination key and door from their collections.
         // Repeat
 
-        func findRoutes(from start: Point,
-                        keepingPath path: [WeightedGraph<Point, Int>.E],
-                        keys: Set<Point>,
-                        doors: [String: Point]) -> [[WeightedGraph<Point, Int>.E]] {
-            guard !keys.isEmpty || !doors.isEmpty else { return [path] }
-
-            let all = graph.findAllBfs(from: start) { keys.contains($0) }
-            let doorIndices = Set(doors.values.compactMap { graph.indexOfVertex($0) })
-            let available = all.filter { $0.count { doorIndices.contains($0.u) || doorIndices.contains($0.v) } == 0 }
-            let availableKeyPoints = available.compactMap { $0.last?.v }.map { graph.vertexAtIndex($0) }
-            let remainingPaths: [[[WeightedGraph<Point, Int>.E]]] = zip(available, availableKeyPoints).map { input in
-                let (newPath, keyPoint) = input
-                let key = navigablePoints[keyPoint]!
-                let door = key.uppercased()
-                let newKeys = keys.subtracting([keyPoint])
-                var newDoors = doors
-                newDoors.removeValue(forKey: door)
-                let additionalRoutes = findRoutes(from: keyPoint, keepingPath: newPath, keys: newKeys, doors: newDoors)
-
-                return additionalRoutes
-            }
-//            let remainingPaths: [[[WeightedGraph<Point, Int>.E]]] = availableKeyPoints.map { keyPoint in
+//        func findRoutes(from start: Point,
+//                        keepingPath path: [WeightedGraph<Point, Int>.E],
+//                        keys: Set<Point>,
+//                        doors: [String: Point]) -> [[WeightedGraph<Point, Int>.E]] {
+//            guard !keys.isEmpty || !doors.isEmpty else { return [path] }
+//
+//            let all = graph.findAllBfs(from: start) { keys.contains($0) }
+//            let doorIndices = Set(doors.values.compactMap { graph.indexOfVertex($0) })
+//            let available = all.filter { $0.count { doorIndices.contains($0.u) || doorIndices.contains($0.v) } == 0 }
+//            let availableKeyPoints = available.compactMap { $0.last?.v }.map { graph.vertexAtIndex($0) }
+//            let remainingPaths: [[[WeightedGraph<Point, Int>.E]]] = zip(available, availableKeyPoints).map { input in
+//                let (newPath, keyPoint) = input
 //                let key = navigablePoints[keyPoint]!
 //                let door = key.uppercased()
 //                let newKeys = keys.subtracting([keyPoint])
 //                var newDoors = doors
 //                newDoors.removeValue(forKey: door)
-//                let additionalRoutes = findRoutes(from: keyPoint, keepingPath: path, keys: newKeys, doors: newDoors)
+//                let additionalRoutes = findRoutes(from: keyPoint, keepingPath: newPath, keys: newKeys, doors: newDoors)
 //
 //                return additionalRoutes
 //            }
-            // For each key point, start the process again.
-            let allPaths = remainingPaths.flatMap { $0 }
-            let fullPaths = allPaths.map { path + $0 }
-
-            return fullPaths
-        }
-
-        let routes = findRoutes(from: atPoint, keepingPath: [], keys: keys, doors: doors)
-//        let description = routes.map { $0.map { "\(graph.vertexAtIndex($0.u)) > \(graph.vertexAtIndex($0.v))" } }
-        let shortestRoute = routes.min { $0.count < $1.count } ?? []
-        let shortestKeys = shortestRoute
-            .map { graph.vertexAtIndex($0.v) }
-            .compactMap { navigablePoints[$0] }
-            .filter { Character($0).isLowercase }
-            .reduce(into: [String]()) { if !$0.contains($1) { $0.append($1) } }
-            .joined(separator: ", ")
-
-        print(shortestKeys)
-        print(shortestRoute.count)
+//            // For each key point, start the process again.
+//            let allPaths = remainingPaths.flatMap { $0 }
+//            let fullPaths = allPaths.map { path + $0 }
+//
+//            return fullPaths
+//        }
+//
+//        let routes = findRoutes(from: atPoint, keepingPath: [], keys: keys, doors: doors)
+        ////        let description = routes.map { $0.map { "\(graph.vertexAtIndex($0.u)) > \(graph.vertexAtIndex($0.v))" } }
+//        let shortestRoute = routes.min { $0.count < $1.count } ?? []
+//        let shortestKeys = shortestRoute
+//            .map { graph.vertexAtIndex($0.v) }
+//            .compactMap { navigablePoints[$0] }
+//            .filter { Character($0).isLowercase }
+//            .reduce(into: [String]()) { if !$0.contains($1) { $0.append($1) } }
+//            .joined(separator: ", ")
+//
+//        print(shortestKeys)
+//        print(shortestRoute.count)
     }
 }
